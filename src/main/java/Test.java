b236378a152ae1c8ba32a54098fadc908983e504
Shaheen Akond <shaheen.akond@roche.com>
2021-07-08 21:06:37 +0200

    Challenge to format with System.out.printf() completed

---
+++ src/main/java/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+
+    public void test(String s1, String s2, String s3, int x, int y, int z) {
+
+        if (s1.length() <= 10 && x >= 0 && x <= 999) {
+            System.out.printf("%-15s%d%n", s1, x);
+            System.out.printf("%-15s%d%n", s2, y);
+            System.out.printf("%-15s%d%n", s3, z);
+        } else {
+            System.out.println("Number is unrecognized!");
+        }
+    }
+}
+
---
+++ src/main/java/myMain.java
@@ -0,0 +1,7 @@
+public class myMain {
+
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.test("java", "compiler", "run",255,50,5);
+    }
+}
