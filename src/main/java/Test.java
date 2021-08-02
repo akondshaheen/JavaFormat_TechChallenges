49525abdfcf8d726c1cbe645c8c639089100150f
AkondsMac <akondsMac@MacBook-Pro-de-AkondsMac.local>
2021-08-02 13:54:16 +0200

    digit functionality update

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,9 +3,9 @@ public class Test {
     public void test(String s1, String s2, String s3, int x, int y, int z) {
 
         if (s1.length() <= 10 && x >= 0 && x <= 999) {
-            System.out.printf("%-15s%d%n", s1, x);
-            System.out.printf("%-15s%d%n", s2, y);
-            System.out.printf("%-15s%d%n", s3, z);
+            System.out.printf("%-15s%03d%n", s1, x);
+            System.out.printf("%-15s%03d%n", s2, y);
+            System.out.printf("%-15s%03d%n", s3, z);
         } else {
             System.out.println("Number is unrecognized!");
         }
